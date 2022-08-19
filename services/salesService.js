d0b4cb5c24a46132772d8801b8adc87f795b8688
hsbicalho <hsbicalho@gmail.com>
2022-08-19 12:30:41 -0300

    requisito 6 implementado e funcionando

--- services/salesService.js
+++ services/salesService.js
@@ -2,13 +2,15 @@ const salesModel = require('../models/SalesModels');
 const getProducts = require('../models/ProductsModels');
 
 const postSales = async (saleData) => {
-  const product = await saleData.map(({ productId }) => getProducts.getById(productId));
-  if (product.length === 0) throw new Error();
-  const { id } = await salesModel.postSales();
-  console.log(id);
+  const [result] = await Promise.all(saleData.map(({ productId }) =>
+    getProducts.getById(productId)));
+  if (result.some((array) => array.length === 0)) {
+ const { id } = await salesModel.postSales();
     const sales = await Promise.all(saleData.map(({ productId, quantity }) =>
     salesModel.postSalesProducts({ saleId: id, productId, quantity })));
   return { id, itemsSold: sales };
+  }
+  throw new Error();
 };
 
 module.exports = { postSales };
